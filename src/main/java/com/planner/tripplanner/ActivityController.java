1e3fd6c77525bc76d2c8f83c514b1893e2a6922f
magikitty <elinaw@fastmail.com>
2022-11-26 15:00:51 +0200

    Implement form for adding new activities

--- src/main/java/com/planner/tripplanner/ActivityController.java
+++ src/main/java/com/planner/tripplanner/ActivityController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 
@@ -16,7 +18,7 @@ public class ActivityController {
     @GetMapping("/activities")
     public String getActivities(Model model) {
         // Add example activities to database
-        this.activityService.addExampleActivities();
+//        this.activityService.addExampleActivities();
         ArrayList<Activity> activitiesList = activityService.getActivities();
         // Add activities to model for front end
         model.addAttribute("activitiesList", activitiesList);
@@ -29,5 +31,11 @@ public class ActivityController {
         Activity activity = activityService.getActivity(Long.valueOf(idActivity));
         model.addAttribute("activity", activity);
         return "activityPage";
+    }
+
+    @PostMapping("/activities")
+    public String addActivity(@RequestParam String activityName) {
+        activityService.addActivity(activityName);
+        return "redirect:/activities";
     }
 }
